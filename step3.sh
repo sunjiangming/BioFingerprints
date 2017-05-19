3f5a36708e58645a4f4fb530e296f761c7b6589d
Jiangming Sun <sunjiangming@gmail.com>
2017-05-19 13:29:42 +0200

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -10,9 +10,9 @@ mkdir -p $2/htsfp
 
 while read -r cnt aid cols op others;
 do
-	if [ -s "files/concise/$aid.concise.tsv" ]
+	if [ -s "$2/concise/$aid.concise.tsv" ]
     then
-      Rscript --vanilla compute_biofingerprints_within_assay.R "$2/files/concise/$aid.concise.tsv" "$2/htsfp/$aid.htsfp.txt"
+      Rscript --vanilla compute_biofingerprints_within_assay.R "$2/concise/$aid.concise.tsv" "$2/htsfp/$aid.htsfp.txt"
       if [ -s "htsfp/$aid.htsfp.txt" ]
       then
         Rscript --vanilla hit_rate.R "$2/htsfp/$aid.htsfp.txt" "$2/htsfp/$aid.htsfp.hitrate.tsv"
